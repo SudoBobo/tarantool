354e7e4d348b274f1d3dbc8b5eded24c7f167c72
AKhatskevich <avkhatskevich@tarantool.org>
2018-02-01 13:33:40 +0300

    sql: fix segfault on table rename failure

--- src/box/sql.c
+++ src/box/sql.c
@@ -886,8 +886,9 @@ int tarantoolSqlite3RenameTable(int iTab, const char *new_name, char **sql_stmt)
 	return SQLITE_OK;
 
 rename_fail:
+	sqlite3Error(db, SQLITE_ERROR);
 	box_iterator_free(iter);
-	return SQLITE_TARANTOOL_ERROR;
+	return SQLITE_ERROR;
 }
 
 /*
@@ -977,8 +978,9 @@ int tarantoolSqlite3RenameParentTable(int iTab, const char *old_parent_name,
 	return SQLITE_OK;
 
 rename_fail:
+	sqlite3Error(db, SQLITE_ERROR);
 	box_iterator_free(iter);
-	return SQLITE_TARANTOOL_ERROR;
+	return SQLITE_ERROR;
 }
 
 
